9beda7147464d4df4288f1a36b1c1a7aec4fd812
zuriros <hanami33rosas@gmail.com>
2018-11-20 13:04:04 -0500

    avanzando con el proyecto

---
+++ src/index.js
@@ -0,0 +1,3 @@
+document.getElementById('entrada').addEventListener("click");
+document.getElementById('misterio').addEventListener("enter");
+document.getElementById('fin').addEventListener("click");
