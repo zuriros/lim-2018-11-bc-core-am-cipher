485cf752a34fabded7a61ec82f9ba03068800dae
zuriros <hanami33rosas@gmail.com>
2018-11-23 17:41:10 -0500

    avanzando js2

--- src/index.js
+++ src/index.js
@@ -1,3 +1,20 @@
-document.getElementById('entrada').addEventListener("click");
-document.getElementById('misterio').addEventListener("enter");
-document.getElementById('fin').addEventListener("click");
+const ambos =document.getElementById('mensaje');
+const numb= document.getElementById('password');
+const btnCodigo = document.getElementById('misterio');
+const boxResult= document.getElementById('resultado');
+
+function cifrar(string,offset) {
+  let cifrada="";
+for (var i = 0; i < string.length; i++) {
+    ascii=string.charCodeAt(i);
+    formula=(ascii-65+offset)%26+65;
+    cifrada += String.fromCharCode(formula);
+}
+  return cifrada;
+}
+
+
+
+btnCodigo.addEventListener('click', () => {
+  boxResult.innerHTML = cifrar(ambos.value,parseInt(numb.value));
+})
