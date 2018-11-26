480bf9655948df6aa5007dc3a92a6582b1880cf0
zuriros <hanami33rosas@gmail.com>
2018-11-26 18:05:43 -0500

    descifrar y cifrar

--- src/cipher.js
+++ src/cipher.js
@@ -1,3 +1,43 @@
 window.cipher = {
-  // ... 
+  // ...
 };
+
+function cipher1(string,offset) {
+  let blankEspace = "";
+  for (let i = 0; i < string.length; i++) {
+    codCipher=string.charCodeAt(i);
+//     if (codCipher>=65 && codCipher<=90) {
+//     offsetformula=(codCipher - 65 + offset)%26+65;
+//     letterCod= String.fromCharCode(offsetformula);
+//   // OTRA FORMA DE CONCATENAR blankEspace+= String.fromCharCode(offsetformula);
+//  blankEspace+=letterCod;
+// }
+//      else if (codCipher>=97 && codCipher<=122) {
+//      offsetFormulaLower=(codCipher-97+offset)%26+97;
+//      letterCodLower=String.fromCharCode(offsetFormulaLower);
+//      blankEspace+=letterCodLower;
+//      }
+// else if (codCipher==32) {
+//        hole=" "
+//     blankEspace+=hole;
+// }
+//  }
+//  return blankEspace;
+//   }
+  // Empezando con el descirado
+
+
+function decipher1(string,offset) {
+  let blankSpaceSec = "";
+  for (var i = 0; i < string.length; i++) {
+    codDecipher = string.charCodeAt(i);
+    offsetDez=(codDecipher+65-offset)%26+65;
+    letterDecode=String.fromCharCode(offsetDez);
+    blankSpaceSec+=letterDecode;
+if (codDecipher>=97 && codDecipher<=122) {
+  offsetDezLower=
+
+}
+}
+return blankSpaceSec;
+}
--- src/index.js
+++ src/index.js
@@ -1,20 +1,15 @@
 const firstText =document.getElementById('mensaje');
-const numbCod= document.getElementById('codigo');
+const numbCod= document.getElementById('enciph');
 const secondText = document.getElementById('mensaje2');
 const passwordInput= document.getElementById('numb');
+const numbCodTwo=document.getElementById('deciph');
+
 
 numbCod.addEventListener('click', () => {
-  secondText.innerHTML = cipher1(firstText.value,parseInt(passwordInput.value))
+  secondText.innerHTML = cipher1(firstText.value,parseInt(passwordInput.value));
 });
 
 
-function cipher1(string,offset) {
-  let blankEspace=""
-for (var i = 0; i < string.length; i++) {
-    codCipher=string.charCodeAt(i);
-    offsetformula=(codCipher-65+offset)%26+65;
-    letterCod= String.fromCharCode(offsetformula);
-    blankEspace+=letterCod;
-}
-  return blankEspace;
-}
+numbCodTwo.addEventListener('click',() =>{
+  secondText.innerHTML = decipher1(firstText.value,parseInt(passwordInput.value));
+});
